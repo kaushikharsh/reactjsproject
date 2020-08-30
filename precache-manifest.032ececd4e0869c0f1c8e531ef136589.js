b314eaa3cac943af0620dd8c324d5337b146dcbb
kaushikharsh <51286888+kaushikharsh@users.noreply.github.com>
2020-08-31 03:19:34 +0530

    Updates

---
+++ precache-manifest.032ececd4e0869c0f1c8e531ef136589.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c8a262818555ec53bf6b138a32b752",
+    "url": "/reactjsproject/index.html"
+  },
+  {
+    "revision": "c9f32463503d2e69795b",
+    "url": "/reactjsproject/static/css/2.864ae908.chunk.css"
+  },
+  {
+    "revision": "ab7d81b9ccbe9ab14f63",
+    "url": "/reactjsproject/static/css/main.108a2926.chunk.css"
+  },
+  {
+    "revision": "c9f32463503d2e69795b",
+    "url": "/reactjsproject/static/js/2.e21d0e75.chunk.js"
+  },
+  {
+    "revision": "dac3c8007ea620c252041370c7e94b06",
+    "url": "/reactjsproject/static/js/2.e21d0e75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab7d81b9ccbe9ab14f63",
+    "url": "/reactjsproject/static/js/main.45e33d40.chunk.js"
+  },
+  {
+    "revision": "e10d611549d6a82a48f0",
+    "url": "/reactjsproject/static/js/runtime-main.021cfe11.js"
+  },
+  {
+    "revision": "40f4e00e791f2bf4c27957bf7905d69b",
+    "url": "/reactjsproject/static/media/bbx2.40f4e00e.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/reactjsproject/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/reactjsproject/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/reactjsproject/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/reactjsproject/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/reactjsproject/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "298c80655705362c35db0ef0a903b798",
+    "url": "/reactjsproject/static/media/myphoto.298c8065.jpg"
+  },
+  {
+    "revision": "94ec10e71611211c9522781efd25ff60",
+    "url": "/reactjsproject/static/media/natak.94ec10e7.jpg"
+  },
+  {
+    "revision": "e525c961de733592deff2d657c4efa9e",
+    "url": "/reactjsproject/static/media/project1.e525c961.jpg"
+  },
+  {
+    "revision": "01c100e0f024ab4594208dcc140d6347",
+    "url": "/reactjsproject/static/media/resume.01c100e0.pdf"
+  }
+]);
